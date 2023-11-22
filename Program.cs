287dedd822056b8f69afc90faf65fd64bd960b93
Rashid <rashid_74@mail.ru>
2023-11-22 15:38:21 +0300

    Добавлена текстовая информация в readme

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Создает  из имеющегося массива строк array1 формирует массив array2 из строк, 
+        //длина которых меньше либо равна 3 символа.
+        void SecondArray(string[] array1, string[] array2)
+        {
+            int count = 0;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i].Length <= 3)
+                {
+                    array2[count] = array1[i];
+                    count++;
+                }
+            }
+        }
+        //Выводит на печать массив array
+        void PrintArray(string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+        }
+        string[] arrOne = new string[6] { "789", "45", "qwerty", "war", "insert", "R" };
+        string[] arrTwo = new string[arrOne.Length];
+        SecondArray(arrOne, arrTwo);
+        PrintArray(arrOne);
+        PrintArray(arrTwo);
+    }
+}
